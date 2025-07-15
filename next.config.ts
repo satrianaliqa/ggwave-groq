45cdeaf0142b251c08daad662878b2eb36b10540
satrianaliqa <satrianaliqa0805@gmail.com>
2025-07-15 16:43:22 +0000

    Add next.config.js with WebAssembly support configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,1 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-    return config;
-  },
-};
-
-export default nextConfig;
